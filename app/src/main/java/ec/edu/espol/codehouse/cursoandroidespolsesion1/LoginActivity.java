29247769b26941178211e667d2d3fc3ee83a9a0a
jjcrowHawk <jjcrow@espol.edu.ec>
2018-08-22 20:14:43 -0500

    adding comments

--- app/src/main/java/ec/edu/espol/codehouse/cursoandroidespolsesion1/LoginActivity.java
+++ app/src/main/java/ec/edu/espol/codehouse/cursoandroidespolsesion1/LoginActivity.java
@@ -10,30 +10,78 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /*Estos objetos serviran para enlazar los elementos de la ventana activity_logEin.xml*/
     Button btn_login;
     EditText et_usuario, et_password;
 
+    /*Este metodo se ejecuta cuando se crea o se inicializa la ventana del activity.
+      Es en este metodo donde enlazamos los objetos con los elementos de la ventana y
+      donde se declaran los eventos para los elementos que van a interactuar con el usuario
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*En esta linea se indica que ventana es la que se va a cargar.
+          No olvidar que para referenciar el archivo layout (ventana) se
+          debe utilizar la referencia: R.layout.nombre_archivo_layout*/
         setContentView(R.layout.activity_login);
 
+        /*Aqui se enlazan los objetos de java con los elementos de la ventana o layout
+          el metodo que hace el enlace es findViewById() y recibe como parametro el id
+          del elemento (revisar el atributo id en el editor de layouts). Para acceder al
+          id del elemento debemos usar la referencia: R.id.id_elemento*/
         et_usuario= (EditText) findViewById(R.id.et_usuario);
         et_password= (EditText) findViewById(R.id.et_password);
         btn_login= (Button) findViewById(R.id.btn_login);
 
+        /*Aqui se declara la accion que debe realizar el boton cuando el usuario lo presione.
+          el metodo recibe un objeto Listener de una clase anonima, lo que quiere decir que
+          siempre tendremos que implementar el metodo de esa clase anonima, que en este caso
+          es el metodo onClick()
+         */
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 System.out.println("Boton TOCADO!!!");
+
+                /*Como los objetos de java ya estan enlazados a los elementos del layout, es posible
+                  extraer algunos atributos de estos elementos. En este caso, como los objetos EditText et_usuario
+                  y et_password estan enlazados a los EdiText Usuario y Contrasena, podemos extraer
+                  el texto que se ha escrito en esos EditText con el metodo getText(), el cual
+                  devuelve un objeto de tipo Editable, el cual es un String editable. Si queremos
+                  obtener el texto de este objeto, basta con aplicarle el metodo toString().
+                 */
                 String usuario= et_usuario.getText().toString();
                 String password= et_password.getText().toString();
+
+                /*Se desafia a que ahora comparen esto con una lista de usuarios y contrasenas predefinidas*/
                 if(usuario.equals("admin") && password.equals("1234")){
-                    //Toast.makeText(LoginActivity.this, "USUARIO VALIDO!", Toast.LENGTH_SHORT).show();
+                    /*La clase Toast nos permite mostrar mensajes de texto al usuario por pantalla.
+                      Esto es posible utilizando el metodo estatico makeText(), el cual recibe 3
+                      parametros: context (La actividad de donde se lo esta invocando), text (El texto
+                      que queremos mostrar y duracion (si deseamos una duracion corta o larga, esto
+                      se lo especifica con los valores estaticos de Toast .LENGTH_SHORT o .LENGHT_LONG
+                    *
+                    Toast.makeText(LoginActivity.this, "USUARIO VALIDO!", Toast.LENGTH_SHORT).show();
+
+
+                    /*La clase Intent nos permite hacer una transicion de una actividad a otra.
+                      el constructor de esta clase recibe 2 parametros: el contexto (context) de
+                      la actividad que lo llama y la actividad hacia donde se dirige (esta se especifica
+                      con el nombre e invocando el atributo class.
+                     */
                     Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+
+                    /*los intents tambien puede enviar informacion desde una actividad a otra,
+                      usando el metodo putExtra, el cual recibe 2 parametros: la clave con el cual
+                      se identifica el objeto y el objeto que se quiere enviar, es decir se envia
+                      un par clave - valor, como si se tratara de un diccionario o Map
+                    */
                     intent.putExtra("usuario",usuario);
+
+                    //Con este metodo ocurre la transicion entre activities, pasando el intent como parametro
                     startActivity(intent);
-                    //LoginActivity.this.finish();
                 }
                 else{
                     Toast.makeText(LoginActivity.this, "USUARIO INVALIDO! Ingrese de nuevo", Toast.LENGTH_SHORT).show();
--- app/src/main/java/ec/edu/espol/codehouse/cursoandroidespolsesion1/MainActivity.java
+++ app/src/main/java/ec/edu/espol/codehouse/cursoandroidespolsesion1/MainActivity.java
@@ -6,19 +6,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*Este objeto se enlazara con el TextView que se encuentra en el layout activity_main.xml*/
     TextView tv_bienvenida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*En esta linea se indica que ventana es la que se va a cargar.
+          No olvidar que para referenciar el archivo layout (ventana) se
+          debe utilizar la referencia: R.layout.nombre_archivo_layout*/
         setContentView(R.layout.activity_main);
 
-        //Toast.makeText(this, "Bienvenido, "+usuario, Toast.LENGTH_SHORT).show();
-
+        /*Se realiza el enlace del objeto al elemento del layout mediante su id*/
         tv_bienvenida = (TextView) findViewById(R.id.tv_bienvenida);
 
+        /* Cuando una activity envia datos a otra activity con el intent por medio del
+           metodo putExtra, la actividad que RECIBE el intent puede extraer los datos por
+           medio de ESTE INTENT utilizando la misma CLAVE con la que se enviaron estos datos.
+           En este caso queremos obtener el String con el nombre de usuario, y para eso en el
+           metodo getStringExtra() pasamos como clave la palabra "usuario", ya que esta palabra
+           fue la clave con la que la se envio este dato.
+         */
         String usuario= getIntent().getStringExtra("usuario");
 
+        /*Ahora especificamos el texto que va a tener el textView de bienvenida*/
         tv_bienvenida.setText("Bienvenido, " + usuario);
+
+        /*--------------------AQUI VA EL CODIGO DE LA SEGUNDA SESION-------------------------*/
+
     }
 }
